debbb9b6f02e560a8dc26fa96c30f5043021a1b4
ObamaOye <m.obamaoye3006@gmail.com>
2022-10-31 14:08:08 +0100

    Mi primer commit!!

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author delfy
+ *
+ */
+module tutorial {
+}
---
+++ src/tutorial/HolaMundo.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Homa mundo CMEPPS!");
+		System.out.println("Bienvenidos al curso");
+
+	}
+
+}
