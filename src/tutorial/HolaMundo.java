8a9205fcab6ebea3a7a476b74784b7ca76801779
ObamaOye <m.obamaoye3006@gmail.com>
2022-10-31 14:59:53 +0100

    Muestro mi nombre

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -5,6 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Homa mundo CMEPPS!");
 		System.out.println("Bienvenidos al curso");
+		
+		String name = "Delfy";
+		System.out.println("Hola ," +name);
 
 	}
 
